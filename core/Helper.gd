8235d3064f72eac4823677a9a44a107739e77326
Caro <74654064+CarolineRoemer1998@users.noreply.github.com>
2026-04-09 14:35:20 +0200

    added grid with tiles; implemented player movement on grid

---
+++ core/Helper.gd
@@ -0,0 +1,21 @@
+extends Node
+
+const LAYER_BIT_PLAYER := 0
+const LAYER_BIT_TILE := 1
+const LAYER_BIT_PLANT := 2
+const LAYER_BIT_POLLEN := 3
+
+var layer_mask := (1 << LAYER_BIT_PLAYER) | (1 << LAYER_BIT_TILE) | (1 << LAYER_BIT_PLANT) | (1 << LAYER_BIT_POLLEN)
+
+func get_position_from_tile(tile: Vector2) -> Vector2:
+	return Vector2(96-16, 24-16) + tile*16
+
+func get_collision_on_area(_position: Vector2, world : World2D) -> Array[Dictionary]:
+	var space = world.direct_space_state
+	var query = PhysicsPointQueryParameters2D.new()
+	query.position = _position
+	query.collision_mask = layer_mask
+	query.collide_with_areas = true
+	return space.intersect_point(query, 5)
+
+#func get_collisions_on_tile()

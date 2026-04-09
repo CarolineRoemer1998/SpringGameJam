16a34b8cca99da4129202e31414be68f5333e65b
cora_lyn <caroroemer@gmx.de>
2026-04-09 16:20:54 +0200

    Signal emitted when player finishes step

--- features/player/player.gd
+++ features/player/player.gd
@@ -44,6 +44,7 @@ func move_player(delta: float):
 		global_position = target_position
 		sprite.play("idle")
 		delta_time = 0.0
+		SignalBus.stepped.emit()
 
 func handle_direction_input():
 	if Input.is_action_pressed("move_up"):

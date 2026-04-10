ecfa2c7498397081049950664c7367e2e80bea5a
markusmeier345 <markus.meier345@gmail.com>
2026-04-10 10:42:29 +0200

    Stats tracker and signals (signals are not being emitted yet)

--- core/SignalBus.gd
+++ core/SignalBus.gd
@@ -3,3 +3,5 @@ extends Node
 signal stepped()
 signal plant_changed_state(plant:Plant , state:Enums.plantStates)
 signal seed_planted(pos: Vector2, plant_type: String) ##TODO: plant_type später zu enum oder ressource-type ändern
+signal plant_collected()
+signal sneezed()
--- core/StatsTracker.gd
+++ core/StatsTracker.gd
@@ -10,16 +10,37 @@ extends Node
 var Stats = {
 	"Steps" : 0,
 	"Plants Collected" : 0,
-	"Plants that died" : 0,
+	"Dead Plants" : 0,
 	"Sneezes" : 0,
 	"Final Score" : 0,
 }
 
 func _ready() -> void:
 	SignalBus.stepped.connect(track_steps)
+	SignalBus.plant_collected.connect(track_plant_collection)
+	SignalBus.plant_changed_state.connect(track_plant_states)
+	SignalBus.sneezed.connect(track_sneezes)
+	SignalBus.plant_collected.connect(track_plant_collection)
 	
 func track_steps():
 	var steps = Stats.get("Steps")
 	steps += 1
 	Stats.set("Steps", steps)
-	print("steps: ", Stats.get("Steps"))
+
+func track_plant_collection():
+	var plants_collected = Stats.get("Plants Collected")
+	plants_collected += 1
+	Stats.set("Plants Collected", plants_collected)
+
+func track_plant_states(plant: Plant, state: Enums.plantStates):
+	match state:
+		Enums.plantStates.DEAD:
+			var dead_plants = Stats.get("Dead Plants")
+			dead_plants += 1
+			Stats.set("Steps", dead_plants)
+	
+func track_sneezes():
+	var sneezes = Stats.get("Sneezes")
+	sneezes += 1
+	Stats.set("Sneezes", sneezes)
+	

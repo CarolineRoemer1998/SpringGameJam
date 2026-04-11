0c9a35a932b63c6948f2a978a120009df713829e
markusmeier345 <markus.meier345@gmail.com>
2026-04-11 22:28:22 +0200

    stats screen

--- core/SignalBus.gd
+++ core/SignalBus.gd
@@ -6,3 +6,5 @@ signal plant_changed_state(plant:Plant , state:Enums.plantStates)
 signal seed_planted(pos: Vector2, plant_type: String) ##TODO: plant_type später zu enum oder ressource-type ändern
 signal flower_collected(flower: Plant)
 signal sneezed()
+
+signal updated_stats(stats:Dictionary)
--- core/StatsTracker.gd
+++ core/StatsTracker.gd
@@ -25,11 +25,13 @@ func track_steps(position: Vector2):
 	var steps = Stats.get("Steps")
 	steps += 1
 	Stats.set("Steps", steps)
+	SignalBus.updated_stats.emit(Stats)
 
 func track_plant_collection(_flower:Plant):
 	var plants_collected = Stats.get("Plants Collected")
 	plants_collected += 1
 	Stats.set("Plants Collected", plants_collected)
+	SignalBus.updated_stats.emit(Stats)
 
 func track_plant_states(plant: Plant, state: Enums.plantStates):
 	match state:
@@ -37,9 +39,10 @@ func track_plant_states(plant: Plant, state: Enums.plantStates):
 			var dead_plants = Stats.get("Dead Plants")
 			dead_plants += 1
 			Stats.set("Steps", dead_plants)
+			SignalBus.updated_stats.emit(Stats)
 	
 func track_sneezes():
 	var sneezes = Stats.get("Sneezes")
 	sneezes += 1
 	Stats.set("Sneezes", sneezes)
-	
+	SignalBus.updated_stats.emit(Stats)
